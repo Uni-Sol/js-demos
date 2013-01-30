15a76e4d6c51d524c7705a7f967ac6e57dff7f7a
Revlin John <stylogicalmaps@gmail.com>
2013-01-30 02:15:53 -0700

    mov16

--- scripts/mov16.js
+++ scripts/mov16.js
@@ -1,8 +1,9 @@
 var data=0, pc=0;
 
-var value = function v() {
-  var p = 32 -(v.pc*8+16);
-  return ( ( data<<p )>>>( p ) )>>>( v.pc*8 );
+var value = function r(d) {
+  var p = 32 -(r.pc*8+16);
+  if (d) data = ( data<<(r.pc*8)>>>(r.pc*8) ) + ( d<<(r.pc*8) );
+  return ( ( data<<p )>>>( p ) )>>>( r.pc*8 );
 };
 
 var result = function r(d) {
@@ -13,21 +14,32 @@ var result = function r(d) {
 
 if ( (process) && (process.argv[2]) ) {
   data=( (data<<16) + process.argv[2] );
-  console.log("Value: "+ value()  +"\n");
+  console.log( "DATA: "+ data +"\n" );
   pc = mov16( pc, value, result );
-  console.log("Result: "+ result() +"\n");
-  console.log("Program Counter: "+ pc +"\n");
-}
+  console.log( "DATA: "+ data +"\n" );
+} else console.log( "Please provide an int argument as input data.\n" );
+
 
 function mov16( pc, value, result ) {
+  console.log("Program Counter: "+ pc +"\n");
 
   value.pc = pc;
   pc += 2;
+  
+  console.log("VALUE: "+ value()  +" at Offset:"+ value.pc +"\n");
+  console.log("Program Counter: "+ pc +"\n");
 
   result.pc = pc;
   pc += 2;
+  
+  console.log("RESULT: "+ result() +" at Offset:"+ result.pc +"\n");
+  console.log("Program Counter: "+ pc +"\n");
 
+  console.log("Moving 16b value \n");
   result(value());
+  
+  console.log("VALUE: "+ value()  +" at Offset:"+ value.pc +"\n");
+  console.log("RESULT: "+ result() +" at Offset:"+ result.pc +"\n");
   
   return pc;
 }

89b621cac813fa45616a345d85aa05994d1e21af
Revlin John <stylogicalmaps@gmail.com>
2013-05-19 15:03:06 -0600

    update processing

--- processing/tunnel3d/tunnel3d.java
+++ processing/tunnel3d/tunnel3d.java
@@ -13,7 +13,7 @@ public class tunnel3d extends PApplet {
   public void setup() { 
     size(w*3,w);
     svg = (P8gGraphicsSVG) createGraphics(width, height, P8gGraphicsSVG.SVG, "tunnel3d.svg");
-    noLoop();
+    //noLoop();
     noStroke(); 
     c=0;
     uPv=new int[w*w]; 
@@ -39,30 +39,43 @@ public class tunnel3d extends PApplet {
 
   public void draw() {
     if( s == 0 ) beginRecord(svg);
+    
 
     //System.out.println("s is "+ (s&w) +"\n");
     //System.out.println("mouseY is "+ mouseY +"\n");
     //System.out.println("mY/w is "+(mouseY/(w*0.5)) +"\n");
-    for(y=0;y<w;y++){ 
-      for(x=0;x<w;x++){  
-        fill( color((s+uPv[y*w+x])&0xFF) );
-        rect( x, y, 1, 1 ); 
-        fill( color((u2lv[y*w+x])&0xFF) );
-        rect( x+w, y, 1, 1 );
-        fill( color(((s+uPv[y*w+x])^(int)((1.0-mouseY/(w*0.5)) * u2lv[y*w+x]))&0xFF) );
-        rect( x+w2, y, 1, 1 );
-        //set(x+w2,y,color(t[((s&w)*w*w)+(y*w)+x])); 
-      }  
+    int fct = 250; // Scale factor; smaller number == bigger rows; must be less than 255
+    int ofs = 4; // Start point; must be less than 255-fct
+    strokeWeight(1);
+    for(y=ofs;y<(fct+ofs);y++){
+      noStroke();      
+      for(x=0;x<w;x++){
+        if(! spress ) {
+          fill( color((s+uPv[y*w+x])&0xFF, 0, 0) );
+          rect( x, (y-ofs)*(w/fct), 2, w/fct ); 
+          fill( color(0, 0, (u2lv[y*w+x])&0xFF) );
+          rect( x+w, (y-ofs)*(w/fct), 2, w/fct );
+        }
+        //fill( color((s+uPv[y*w+x])&0xFF, 0, (u2lv[y*w+x])&0xFF) );
+        //rect( x+w2, (y-ofs)*(w/fct), 2, w/fct );
+        set( x+w2, (y-ofs)*(w/fct), color((s+uPv[y*w+x])&0xFF, 0, (u2lv[y*w+x])&0xFF) );
+      } 
+      if( fct < 64 ) {
+        stroke(255);
+        line( 0, (y-ofs)*(w/fct), w*3, (y-ofs)*(w/fct) );
+      }
     }
-    s++;
+    if( spress ) {
+       s++;
+       println(s);
+    }
     if( s == 0 ) {
        println( s );
        endRecord();
        println( "Done drawing\n" );
     }
-    if( spress ) s = 0;
-    if( s == 127 ) {
-      save("Tunnel.s127.png");
+    if( s == 64 ) {
+      save("Tunnel.s64.png");
     }
   }
 

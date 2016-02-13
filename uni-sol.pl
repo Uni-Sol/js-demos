62623ae578696a507964e8e4c704d0b4b036af04
John <john@real-currents.com>
2016-02-13 10:15:48 -0800

    update vision

--- uni-sol.pl
+++ uni-sol.pl
@@ -64,7 +64,7 @@ get '/vision' => sub {
 		header => "Share The Dream",
 		canvasApp => '/js-demos/scripts/interact-visualizer.js'
 	);
-	$self->stash( mediaDIV => '<div id="stream" style="text-align: center"><br /><audio id="aud1" preload="auto" controls="true"><source src="/js-demos/audio/stymaps.mp3" /><source src="/js-demos/audio/stymaps.ogg" /></audio></div>' );
+	$self->stash( mediaDIV => '<div id="stream" style="text-align: center"><br /><audio id="aud1" preload="auto" controls="true"><source src="/js-demos/audio/morning-01.mp3" /><source src="/js-demos/audio/morning-01    .ogg" /></audio></div>' );
 	$self->render('vision');
 };
 
